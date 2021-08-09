63b57723112a43577cab9d71842d56be3f926453
Giovanni Franco Calfiqueo <giovanni.franco@live.cl>
2021-08-09 17:26:27 -0400

    Cambios

--- controldent/app/assets/javascripts/application.js
+++ controldent/app/assets/javascripts/application.js
@@ -1,2 +1,2 @@
-require jquery3
-require jquery_ujs
+//= require jquery
+//= require jquery_ujs
--- controldent/app/javascript/packs/application.js
+++ controldent/app/javascript/packs/application.js
@@ -2,16 +2,31 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import {} from 'jquery-ujs'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
-import "css/styles"
-
-import "jquery"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("jquery")
+require("@popperjs/core")
+import "bootstrap"
+// import { Modal } from 'bootstrap'
+import { Tooltip, Popover } from 'bootstrap'
+
+jQuery(function() {
+    // alert(123456)
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+})
